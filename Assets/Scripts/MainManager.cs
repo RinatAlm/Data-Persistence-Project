03f57399244d27081b057375f2f5900fc21af8eb
R1TTER_1246 <102379043+RinatAlm@users.noreply.github.com>
2022-07-05 17:13:44 +0500

    Finished
    Everything is working

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainManager : MonoBehaviour
 {
@@ -11,20 +15,78 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text MaxScoreText;
     public GameObject GameOverText;
+    public GameObject StartButton;
+    public GameObject RestartButton;
+    public GameObject ExitButton;
+    public GameObject InputField;
+    public Text InputText;
+    public GameObject SaveButton;
     
     private bool m_Started = false;
     private int m_Points;
-    
+    public int maxPoints;
+    public string maxName;
+   
+
     private bool m_GameOver = false;
 
-    
+    [System.Serializable]
+    class SaveData
+    {
+        public int savedPoints;
+       public string maxName;
+    }
+
+
+   public void SaveScore()
+    {
+        maxName = InputText.text;
+        SaveData data = new SaveData();
+        data.savedPoints = m_Points;
+        data.maxName = maxName;
+     
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/SaveData.json", json);
+    }
+
+
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/SaveData.json";
+        if(File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            maxPoints = data.savedPoints;
+            maxName = data.maxName;
+            
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
+        StartButton = GameObject.Find("StartButton");
+        RestartButton = GameObject.Find("RestartButton");
+        ExitButton = GameObject.Find("ExitButton");
+        InputField = GameObject.Find("InputField");
+        SaveButton = GameObject.Find("SaveButton");
+        InputText = GameObject.Find("Input").GetComponent<Text>();
+        MaxScoreText = GameObject.Find("ScoreText (1)").GetComponent<Text>();
+        ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        RestartButton.SetActive(false);
+        InputField.SetActive(false);
+        SaveButton.SetActive(false);
+        LoadScore();
+
+        MaxScoreText.text = "Best Score: " + maxName + ":" + maxPoints;
+
         int[] pointCountArray = new [] {1,1,2,2,5,5};
         for (int i = 0; i < LineCount; ++i)
         {
@@ -40,11 +102,10 @@ public class MainManager : MonoBehaviour
 
     private void Update()
     {
-        if (!m_Started)
+        if (m_Started)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                m_Started = true;
                 float randomDirection = Random.Range(-1.0f, 1.0f);
                 Vector3 forceDir = new Vector3(randomDirection, 1, 0);
                 forceDir.Normalize();
@@ -71,6 +132,35 @@ public class MainManager : MonoBehaviour
     public void GameOver()
     {
         m_GameOver = true;
+        if(m_Points > maxPoints)
+        {
+            InputField.SetActive(true);
+            SaveButton.SetActive(true);
+        }
         GameOverText.SetActive(true);
+        RestartButton.SetActive(true);
+        
+    }
+
+    public void GameStart()
+    {
+        m_Started = true;
+        StartButton.SetActive(false);
+        ExitButton.SetActive(false);
+        
+    }
+
+    public void GameRestart()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+Application.Quit();
+#endif
     }
 }
